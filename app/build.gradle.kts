2b81f68ea8bba135e23f163e4e90f45694d639f1
enes_yildirim <enes_yildirim@beymen.com>
2024-07-20 16:05:25 +0300

    update targetSdk and hilt version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,8 @@ android {
     defaultConfig {
         applicationId = "com.enons.vehicleapp"
         minSdk = 26
-        targetSdk = 33
+        //noinspection OldTargetApi
+        targetSdk = 34
         versionCode = 3
         versionName = "1.3"
 
@@ -44,19 +45,20 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.9"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -107,8 +109,8 @@ dependencies {
     implementation("com.google.android.gms:play-services-ads:22.6.0")
 
     // Dagger Hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.46")
+    kapt("com.google.dagger:hilt-android-compiler:2.46")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
