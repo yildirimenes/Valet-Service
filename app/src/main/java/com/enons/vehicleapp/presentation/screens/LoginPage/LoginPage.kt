a0c2bad27e2283874ed222b57b5f32960b3228c2
yildirimenes <104741427+yildirimenes@users.noreply.github.com>
2024-08-11 15:43:12 +0300

    update login page

--- app/src/main/java/com/enons/vehicleapp/presentation/screens/LoginPage/LoginPage.kt
+++ app/src/main/java/com/enons/vehicleapp/presentation/screens/LoginPage/LoginPage.kt
@@ -3,120 +3,88 @@ package com.enons.vehicleapp.presentation.screens.LoginPage
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.repeatOnLifecycle
-import kotlinx.coroutines.flow.Flow
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.enons.vehicleapp.navigation.Screen
+import com.enons.vehicleapp.presentation.RegisterPage.viewmodel.AuthState
+import com.enons.vehicleapp.presentation.screens.LoginPage.viewmodel.LoginPageViewModel
 
 @Composable
-fun LoginScreen(
-    uiState: LoginContract.UiState,
-    uiEffect: Flow<LoginContract.UiEffect>,
-    onAction: (LoginContract.UiAction) -> Unit,
-    onNavigateMainScreen: () -> Unit,
+fun LoginPage(
+    modifier: Modifier = Modifier,
+    navController: NavController,
 ) {
+    val viewModel: LoginPageViewModel = hiltViewModel()
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val authState by viewModel.authState.observeAsState()
     val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
-    LaunchedEffect(uiEffect, lifecycleOwner) {
-        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-            uiEffect.collect { effect ->
-                when (effect) {
-                    is LoginContract.UiEffect.ShowToast -> {
-                        Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show()
-                    }
 
-                    is LoginContract.UiEffect.GoToMainScreen -> {
-                        onNavigateMainScreen()
-                    }
-
-                    LoginContract.UiEffect.GoToLoginScreen -> TODO()
-                }
-            }
+    LaunchedEffect(authState) {
+        when (authState) {
+            is AuthState.Authenticated -> navController.navigate(Screen.HomePage.route)
+            is AuthState.Error -> Toast.makeText(
+                context,
+                (authState as AuthState.Error).message, Toast.LENGTH_SHORT
+            ).show()
+            else -> Unit
         }
     }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        EmailAndPasswordContent(
-            email = uiState.email,
-            password = uiState.password,
-            onEmailChange = { onAction(LoginContract.UiAction.ChangeEmail(it)) },
-            onPasswordChange = { onAction(LoginContract.UiAction.ChangePassword(it)) },
-            onSignInClick = { onAction(LoginContract.UiAction.SignInClick) },
-            /*onSignUpClick = { onAction(LoginContract.UiAction.SignUpClick) },*/
+        Text(text = "Login Page", fontSize = 32.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "Email") }
         )
-    }
-}
+        Spacer(modifier = Modifier.height(8.dp))
 
-@Composable
-fun EmailAndPasswordContent(
-    email: String,
-    password: String,
-    onEmailChange: (String) -> Unit,
-    onPasswordChange: (String) -> Unit,
-    onSignInClick: () -> Unit,
-    //onSignUpClick: () -> Unit,
-) {
-    Text(
-        text = "Email / Password",
-        fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
-    )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
 
-    Spacer(modifier = Modifier.height(16.dp))
-
-    OutlinedTextField(
-        value = email,
-        maxLines = 1,
-        placeholder = { Text("Email") },
-        onValueChange = onEmailChange,
-    )
-
-    Spacer(modifier = Modifier.height(16.dp))
-
-    OutlinedTextField(
-        value = password,
-        maxLines = 1,
-        placeholder = { Text("Password") },
-        onValueChange = onPasswordChange,
-    )
-
-    Spacer(modifier = Modifier.height(16.dp))
-
-    Row(
-        horizontalArrangement = Arrangement.Center,
-    ) {
         Button(
-            onClick = { onSignInClick() },
+            onClick = { viewModel.login(email, password) },
+            enabled = authState != AuthState.Loading
         ) {
-            Text("Sign In")
+            Text(text = "Login")
         }
 
-        Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
 
-        Button(
-            onClick = { /*onSignUpClick()*/ },
-        ) {
-            Text("Sign Up")
+        TextButton(onClick = {
+            navController.navigate(Screen.RegisterPage.route)
+        }) {
+            Text(text = "Don't have an account, Signup")
         }
     }
 }
---
+++ app/src/main/java/com/enons/vehicleapp/presentation/screens/LoginPage/viewmodel/LoginPageViewModel.kt
@@ -0,0 +1,23 @@
+package com.enons.vehicleapp.presentation.screens.LoginPage.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.enons.vehicleapp.data.repository.FirebaseAuthRepository
+import com.enons.vehicleapp.presentation.RegisterPage.viewmodel.AuthState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginPageViewModel @Inject constructor(
+    private val authRepository: FirebaseAuthRepository
+) : ViewModel() {
+
+    val authState: LiveData<AuthState> = authRepository.authState
+
+    fun login(email: String, password: String) {
+        authRepository.login(email, password)
+    }
+}
+
+
+
