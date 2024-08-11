af9616731c85b458c18ce15236970eff0f746292
yildirimenes <104741427+yildirimenes@users.noreply.github.com>
2024-08-11 15:46:39 +0300

    add auth repository

---
+++ app/src/main/java/com/enons/vehicleapp/data/repository/FirebaseAuthRepository.kt
@@ -0,0 +1,11 @@
+package com.enons.vehicleapp.data.repository
+
+import androidx.lifecycle.LiveData
+import com.enons.vehicleapp.presentation.RegisterPage.viewmodel.AuthState
+
+interface FirebaseAuthRepository {
+    val authState: LiveData<AuthState>
+    fun login(email: String, password: String)
+    fun signup(email: String, password: String)
+    fun signout()
+}
---
+++ app/src/main/java/com/enons/vehicleapp/data/repository/FirebaseAuthRepositoryImpl.kt
@@ -0,0 +1,64 @@
+package com.enons.vehicleapp.data.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.enons.vehicleapp.presentation.RegisterPage.viewmodel.AuthState
+import com.google.firebase.auth.FirebaseAuth
+import javax.inject.Inject
+
+class FirebaseAuthRepositoryImpl @Inject constructor(
+    private val auth: FirebaseAuth
+) : FirebaseAuthRepository {
+
+    private val _authState = MutableLiveData<AuthState>()
+    override val authState: LiveData<AuthState> = _authState
+
+    init {
+        checkAuthStatus()
+    }
+
+    private fun checkAuthStatus() {
+        _authState.value = if (auth.currentUser == null) {
+            AuthState.Unauthenticated
+        } else {
+            AuthState.Authenticated
+        }
+    }
+
+    override fun login(email: String, password: String) {
+        if (email.isEmpty() || password.isEmpty()) {
+            _authState.value = AuthState.Error("Email or password can't be empty")
+            return
+        }
+        _authState.value = AuthState.Loading
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                _authState.value = if (task.isSuccessful) {
+                    AuthState.Authenticated
+                } else {
+                    AuthState.Error(task.exception?.message ?: "Something went wrong")
+                }
+            }
+    }
+
+    override fun signup(email: String, password: String) {
+        if (email.isEmpty() || password.isEmpty()) {
+            _authState.value = AuthState.Error("Email or password can't be empty")
+            return
+        }
+        _authState.value = AuthState.Loading
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                _authState.value = if (task.isSuccessful) {
+                    AuthState.Authenticated
+                } else {
+                    AuthState.Error(task.exception?.message ?: "Something went wrong")
+                }
+            }
+    }
+
+    override fun signout() {
+        auth.signOut()
+        _authState.value = AuthState.Unauthenticated
+    }
+}
