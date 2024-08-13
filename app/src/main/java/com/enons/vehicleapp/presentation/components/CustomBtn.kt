dbd097c779c20acb6e1e3e8c9f85fe79304976c7
yildirimenes <104741427+yildirimenes@users.noreply.github.com>
2024-08-13 04:31:15 +0300

    add auth text field and button

--- app/src/main/java/com/enons/vehicleapp/presentation/components/CustomBtn.kt
+++ app/src/main/java/com/enons/vehicleapp/presentation/components/CustomBtn.kt
@@ -1,6 +1,7 @@
 package com.enons.vehicleapp.presentation.components
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -16,9 +17,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.enons.vehicleapp.R
 
 @Composable
 fun CustomBtn(
@@ -152,5 +156,28 @@ fun OnBoardingBtn(
             fontSize = fontSize.sp,
             style = textStyle
         )
+    }
+}
+
+@Composable
+fun AuthBtn(
+    onClick: () -> Unit,
+    text: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    containerColor: Color = colorResource(id = R.color.dark_green),
+    contentColor: Color = colorResource(id = R.color.color_3),
+    padding: Dp = 8.dp
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor
+        )
+    ) {
+        Text(text = text, Modifier.padding(vertical = padding))
     }
 }
--- app/src/main/java/com/enons/vehicleapp/presentation/components/CustomOutlinedTextField.kt
+++ app/src/main/java/com/enons/vehicleapp/presentation/components/CustomOutlinedTextField.kt
@@ -1,18 +1,33 @@
 package com.enons.vehicleapp.presentation.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
+import com.enons.vehicleapp.R
 import java.util.Locale
 
 @ExperimentalComposeUiApi
@@ -97,7 +112,9 @@ fun RegisterOutlinedNumberPlateTextField(
 @ExperimentalComposeUiApi
 @Composable
 fun UpdateOutlinedNumberPlateTextField(
-    value: String, onValueChange: (String) -> Unit, label: @Composable () -> Unit
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: @Composable () -> Unit
 ) {
     val maxLength = 15
     OutlinedTextField(
@@ -173,3 +190,90 @@ fun HourlyFeeOutlinedTextField(
         )
     )
 }
+
+@Composable
+fun EmailAuthOutlinedTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: @Composable () -> Unit,
+    leadingIcon: ImageVector,
+    modifier: Modifier = Modifier,
+    singleLine: Boolean = true,
+    focusedTextColor: Color = Color.DarkGray,
+    unfocusedTextColor: Color = Color.DarkGray,
+    focusedContainerColor: Color = Color.Transparent,
+    unfocusedContainerColor: Color = Color.Transparent,
+    focusedIndicatorColor: Color = Color.DarkGray,
+    unfocusedIndicatorColor: Color = Color.LightGray
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = label,
+        modifier = modifier.fillMaxWidth(),
+        leadingIcon = { Icon(imageVector = leadingIcon, contentDescription = null) },
+        singleLine = singleLine,
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = focusedTextColor,
+            unfocusedTextColor = unfocusedTextColor,
+            focusedContainerColor = focusedContainerColor,
+            unfocusedContainerColor = unfocusedContainerColor,
+            focusedIndicatorColor = focusedIndicatorColor,
+            unfocusedIndicatorColor = unfocusedIndicatorColor
+        )
+    )
+}
+
+@Composable
+fun PasswordOutlinedTextField(
+    password: String,
+    onPasswordChange: (String) -> Unit,
+    label: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    passwordVisible: Boolean,
+    onPasswordVisibilityChange: () -> Unit,
+    isPasswordFocused: Boolean,
+    onFocusChange: (FocusState) -> Unit,
+    focusedTextColor: Color = Color.DarkGray,
+    unfocusedTextColor: Color = Color.DarkGray,
+    focusedContainerColor: Color = Color.Transparent,
+    unfocusedContainerColor: Color = Color.Transparent,
+    focusedIndicatorColor: Color = Color.DarkGray,
+    unfocusedIndicatorColor: Color = Color.LightGray
+) {
+    OutlinedTextField(
+        value = password,
+        onValueChange = onPasswordChange,
+        label = label,
+        modifier = modifier
+            .fillMaxWidth()
+            .onFocusChanged { focusState -> onFocusChange(focusState) },
+        leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = null) },
+        trailingIcon = {
+            val image = if (passwordVisible) {
+                painterResource(id = R.drawable.baseline_visibility_24)
+            } else {
+                painterResource(id = R.drawable.baseline_visibility_off_24)
+            }
+
+            Icon(
+                painter = image,
+                contentDescription = if (passwordVisible) "Hide password" else "Show password",
+                modifier = Modifier
+                    .clickable { onPasswordVisibilityChange() }
+                    .alpha(if (isPasswordFocused) 1f else 0f)
+            )
+        },
+        singleLine = true,
+        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = focusedTextColor,
+            unfocusedTextColor = unfocusedTextColor,
+            focusedContainerColor = focusedContainerColor,
+            unfocusedContainerColor = unfocusedContainerColor,
+            focusedIndicatorColor = focusedIndicatorColor,
+            unfocusedIndicatorColor = unfocusedIndicatorColor
+        )
+    )
+}
+
