a7fcb225cfc2c5d50522c5c07e3f2facf6064137
enesyildirim <oyildirim.enes@gmail.com>
2025-08-04 16:43:49 +0300

    release_1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 26
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
